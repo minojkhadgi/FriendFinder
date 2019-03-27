842c2e39c6294617d48db4ca44e962329861da70
minojkhadgi <gwminoj@gmail.com>
2019-03-27 12:02:58 -0400

    changes

---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+var friendsArray = [
+{
+    name:"Doggy",
+    photo:"https://cdn.pixabay.com/photo/2017/12/05/06/39/black-german-shepherd-2998670_960_720.jpg",
+    scores:["1","1","1","1","1","1","1","1","1","1"]
+},
+{
+    name:"owl",
+    photo:"https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_1280.jpg",
+    scores:["5","5","5","5","5","5","5","5","5","5"]
+},
+{
+    name:"lion",
+    photo:"https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045_1280.jpg",
+    scores:["3","1","4","4","2","1","4","5","1","2"]
+},
+{
+    name:"tiger",
+    photo:"https://cdn.pixabay.com/photo/2017/07/24/19/57/tiger-2535888_1280.jpg",
+    scores:["1","2","3","4","5","1","2","3","4","5"]
+},
+{
+    name:"fish",
+    photo:"https://cdn.pixabay.com/photo/2016/12/31/21/22/discus-fish-1943755_1280.jpg",
+    scores:["2","4","1","3","5","2","4","1","3","5"]
+},
+{
+    name:"rabbit",
+    photo:"https://cdn.pixabay.com/photo/2016/12/13/00/13/rabbit-1903016_1280.jpg",
+    scores:["5","3","1","4","2","5","3","1","4","2"]
+},
+{
+    name:"swan",
+    photo:"https://cdn.pixabay.com/photo/2017/02/18/13/55/swan-2077219_1280.jpg",
+    scores:["5","4","3","2","1","5","4","3","2","1"]
+},
+{
+    name:"horse",
+    photo:"https://cdn.pixabay.com/photo/2016/05/25/13/55/horses-1414889_1280.jpg",
+    scores:["3","3","1","1","2","2","4","4","5","5"]
+}];
+module.exports=friendsArray;
