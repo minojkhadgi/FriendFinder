b22bdc023d40d1ebc79ad62df310f3ec29b57ec1
minojkhadgi <gwminoj@gmail.com>
2019-03-31 17:59:31 -0400

    all done

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,8 @@
 var path =require("path");
-module.exports=funtion(app){
-app.use("/",function(req,res){
-    res.sendFile(path.join(__dirname,"../pubic/home.html"));
+module.exports=function(app)
+{
+app.use("/", function(req,res){
+    res.sendFile(path.join(__dirname,"../public/home.html"));
     
 });
 app.get("/survey",function (req,res){
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require("path");
 var bodyParser=require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080;
-
-app.use(express.static(__dirname+"/app/public"));
+//expose the public directory to access css files...
+app.use(express.static(__dirname +"/app/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.text());
@@ -13,6 +13,9 @@ app.use(bodyParser.json({type:"application/vnd.api+json"}));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+app.post("/api/surveys",function(req,res){
+console.log("hi")
+});
 
 
 app.listen(PORT, function() {
